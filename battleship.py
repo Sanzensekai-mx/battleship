314f69a4d70a0be924538b2e7e900ff7da0fbd3a
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2022-10-19 16:12:09 +0400

    Ship class

--- battleship.py
+++ battleship.py
@@ -1,8 +1,6 @@
 class BoardOutException(Exception):
     pass
 
-BOARD_SIZE = 6
-
 # RecursionError: maximum recursion depth exceeded in comparison. You are using the same name for the getter, setter and attribute. 
 # When setting up a property, you must rename the attribute locally; the convention is to prefix it with an underscore.
 
@@ -10,55 +8,43 @@ class Dot:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
-    
-    @x.setter
-    def x(self, value):
-        if value <= BOARD_SIZE and value > 0:
-            self._x = value
-        else:
-            raise ValueError("Координата X не может быть больше размера доски или меньше 0")
-    
-    @y.setter
-    def y(self, value):
-        if value <= BOARD_SIZE and value > 0:
-            self._y = value
-        else:
-            raise ValueError("Координата Y не может быть больше размера доски или меньше 0")
 
     # Сравнение == Dot объектов
     def __eq__(self, other):
         if not isinstance(other, Dot):
             raise TypeError("Один из операндов не относится к типу Dot")
         return self.x == other.x and self.y == other.y
-
+    
+    def __repr__(self):
+        return f"{self.__class__}{self.x, self.y}"
 
 class Ship:
-    def __init__(self, length, ship_bow, ship_direction, lives):
+    def __init__(self, length, ship_bow: Dot, ship_direction):
         self.length = length
         self.ship_bow = ship_bow
         self.ship_direction = ship_direction
-        self.lives = lives
+        self.lives = length
     
-    def dots(self):
+    def __dots(self):
         ship_dots = []
-
-
-        return
+        ship_dots.append(self.ship_bow)
+        if self.length > 1:
+            cur_dot_x, cur_dot_y = self.ship_bow.x, self.ship_bow.y
+            for i in range(self.length - 1):
+                if self.ship_direction == 'vertically':
+                    cur_dot_x += 1
+                elif self.ship_direction == 'horizontally':
+                    cur_dot_y += 1
+                ship_dots.append(Dot(cur_dot_x, cur_dot_y))
+        return ship_dots
 
 class Board:
-    def __init__(self, board_dots, ship_list, hid, alive_ships):
-        self.board_dots = None # !!!!!
-        self.ship_list = ship_list
+    def __init__(self, hid=False, board_size=6):
+        self._board_dots = [[Dot(i, j) for j in range(1, board_size + 1)] for i in range(1, board_size + 1)]
+        self.ship_list = [] # объкты Ships
         self.hid = hid
-        self.alive_ships = alive_ships
+        # self.hid = hid # Тру или фалсе нужно ли скрывать корабли на доске
+        self.alive_ships = 0 # Количество живых кораблей на доске
 
     def add_ship(self):
         pass
@@ -78,6 +64,7 @@ class Board:
 
 class Player:
     def __init__(self):
+        # board = Board
         pass
 
 
@@ -93,5 +80,13 @@ dot2 = Dot(2, 3)
 
 list_dots = [dot2, Dot(1, 5), Dot(5, 3)]
 
-print(dot1 == dot2)
-print(dot1 in list_dots)
+# print(dot1 == dot2)
+# print(dot1 in list_dots)
+
+board = Board()
+
+print(board._board_dots)
+
+ship = Ship(length=3, ship_bow=dot1, ship_direction='vertically')
+
+print(ship._Ship__dots())
