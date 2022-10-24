00eb5e94625ac42a3b351614fcca193edded3f0e
SanZenSekai <45728342+Sanzensekai-mx@users.noreply.github.com>
2022-10-24 13:30:20 +0400

    Доработка Board

--- battleship.py
+++ battleship.py
@@ -1,4 +1,7 @@
-class BoardOutException(Exception):
+class BoardShipException(Exception):
+    pass
+
+class BoardDotOutException(Exception):
     pass
 
 # RecursionError: maximum recursion depth exceeded in comparison. You are using the same name for the getter, setter and attribute. 
@@ -25,7 +28,7 @@ class Ship:
         self.ship_direction = ship_direction
         self.lives = length
     
-    def __dots(self):
+    def dots(self):
         ship_dots = []
         ship_dots.append(self.ship_bow)
         if self.length > 1:
@@ -40,27 +43,60 @@ class Ship:
 
 class Board:
     def __init__(self, hid=False, board_size=6):
-        self._board_dots = [[Dot(i, j) for j in range(1, board_size + 1)] for i in range(1, board_size + 1)]
-        self.ship_list = [] # объкты Ships
+        # self._board_dots = [[Dot(i, j) for j in range(1, board_size + 1)] for i in range(1, board_size + 1)]
+        self._board_dots = [["O"] * board_size for i in range(board_size + 1)]
+        self.ship_list = [] # объекты Ships
         self.hid = hid
+        self.board_size = board_size
         # self.hid = hid # Тру или фалсе нужно ли скрывать корабли на доске
         self.alive_ships = 0 # Количество живых кораблей на доске
+        self.not_empty_dots = []
 
-    def add_ship(self):
-        pass
+    def out(self, dot):
+        return not((self.board_size > dot.x >= 0) and (self.board_size > dot.y >= 0))
 
-    def cotour(self):
-        pass
+    def add_ship(self, ship: Ship):
+        for dot in ship.dots():
+            if self.out(dot):
+                raise BoardShipException(f"Невозможно разместить корабль в точке: {dot.x} {dot.y + 1}" if ship.ship_direction == 'horizontally' 
+                                            else f"Невозможно разместить корабль в точке: {dot.x + 1} {dot.y}")
+            for another_ship in self.ship_list:
+                # print(another_ship.dots())
+                for a_dot in another_ship.dots():
+                    if dot == a_dot:
+                        # print(f'{dot} == {a_dot}')
+                        raise BoardShipException(f"В этой точке уже стоит другой корабль: {dot.x} {dot.y + 1}" if ship.ship_direction == 'horizontally' 
+                                            else f"В этой точке уже стоит другой корабль: {dot.x + 1} {dot.y}")
+        for dot in ship.dots():
+            self._board_dots[dot.x][dot.y] = "■"
+            self.not_empty_dots.append(dot)
+        self.ship_list.append(ship)
+                
 
-    def show_board(self):
-        pass
-
-    def out(self):
-        pass
+    def contour(self, ship: Ship):
+        # ship_contour = []
+        # Использовать out, not_empty_dots
+        for dot in ship.dots():
+            near_dots = [Dot(dot.x + 1, dot.y), Dot(dot.x - 1, dot.y), Dot(dot.x, dot.y + 1),
+                    Dot(dot.x, dot.y - 1), Dot(dot.x + 1, dot.y + 1), Dot(dot.x - 1, dot.y - 1),
+                    Dot(dot.x + 1, dot.y - 1), Dot(dot.x - 1 , dot.y + 1)]
+            for n_dot in near_dots:
+                if self.out(n_dot):
+                    continue
+                if n_dot not in self.not_empty_dots:
+                    self.not_empty_dots.append(n_dot)
+                    self._board_dots[n_dot.x][n_dot.y] = 'T'
+            # self._board_dots[dot.x][dot.y + 1] = "T"
+            # self._board_dots[dot.x][dot.y - 1] = "T"
 
     def shot(self):
         pass
 
+    def __str__(self):
+        result = '  |1|2|3|4|5|6|'
+        for idx, line in enumerate(self._board_dots, 1):
+            result += f"\n{idx} |{'|'.join(line)}|"
+        return result
 
 class Player:
     def __init__(self):
@@ -75,18 +111,38 @@ class User(Player):
 class AI(Player):
     pass
 
-dot1 = Dot(1, 5)
+dot1 = Dot(0, 2)
 dot2 = Dot(2, 3)
 
-list_dots = [dot2, Dot(1, 5), Dot(5, 3)]
+list_dots = [dot2, Dot(0, 5), Dot(5, 3)]
 
 # print(dot1 == dot2)
 # print(dot1 in list_dots)
 
 board = Board()
 
-print(board._board_dots)
+# print(board._board_dots)
 
-ship = Ship(length=3, ship_bow=dot1, ship_direction='vertically')
+ship1 = Ship(length=3, ship_bow=dot1, ship_direction='horizontally')
+ship2 = Ship(length=2, ship_bow=dot2, ship_direction='vertically')
 
-print(ship._Ship__dots())
+# print(ship1.dots())
+
+# board.add_ship(ship1)
+# board.add_ship(ship2)
+
+
+
+dot3 = Dot(2, 3)
+
+ship3 = Ship(length=3, ship_bow=dot3, ship_direction='vertically')
+print(ship3.dots())
+board.add_ship(ship3)
+board.contour(ship3)
+print(board.not_empty_dots)
+
+for row in board._board_dots:
+    print(row)
+# print(board.out(dot3))
+
+print(board)
